ad8f536130d245d9246476ac22494fa713206526
michalziv1 <michalziv89@gmail.com>
2023-11-04 12:34:14 +0100

    Sound Experiment

--- Assets/Scripts/UI_Control/PlayerVoiceInteractionScript.cs
+++ Assets/Scripts/UI_Control/PlayerVoiceInteractionScript.cs
@@ -9,12 +9,14 @@ public class PlayerVoiceInteractionScript : MonoBehaviour
     public GameObject talkingSprite;
     private VoiceCoroutineScript voiceCoroutineScript;
     private bool isCoroutineRunning = false;
-    [SerializeField] private AudioSource audioSource; 
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
 
     void Start()
     {
         voiceCoroutineScript = FindObjectOfType<VoiceCoroutineScript>();
         talkingSprite.SetActive(false);
+        PlayerVoiceLine();
     }
 
     void Update()
@@ -32,6 +34,7 @@ public class PlayerVoiceInteractionScript : MonoBehaviour
             Debug.Log("Played");
             StartCoroutine(PlaySequenceIfNotRunning());
             talkingSprite.SetActive(true);
+            PlayerVoiceLine();
         }
     }
 
@@ -43,5 +46,12 @@ public class PlayerVoiceInteractionScript : MonoBehaviour
             yield return StartCoroutine(voiceCoroutineScript.TheSequence());
             isCoroutineRunning = false;
         }
+    }
+
+    private void PlayerVoiceLine()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play(0);
+        Debug.Log("started");
     }
 }
